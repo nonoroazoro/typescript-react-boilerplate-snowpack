1d7a946ee146c8eaa8c194cce016b5c392b89709
Nonoroazoro <xiaochao.k@gmail.com>
2021-03-17 12:25:12 +0800

    Remove defaultProps

--- src/pages/CounterPage/CounterPage.tsx
+++ src/pages/CounterPage/CounterPage.tsx
@@ -15,7 +15,7 @@ export interface CounterPageProps extends BaseReactProps
 
 export const CounterPage = (props: CounterPageProps) =>
 {
-    const { className, value: initialValue } = props;
+    const { className, value: initialValue = 0 } = props;
     const { throwError } = React.useContext(ErrorBoundaryContext);
 
     const [value, setValue] = React.useState(initialValue);
@@ -58,8 +58,4 @@ export const CounterPage = (props: CounterPageProps) =>
     {
         setShowModal(false);
     }
-};
-
-CounterPage.defaultProps = {
-    value: 0
 };
